6dd3d686161e8845f43e49aac65778c590bb98b7
Fabian <fabian.grun@gmx.de>
2019-01-30 20:58:26 +0100

    "Solving for Carrots" solved

---
+++ Kattis/src/SolvingForCarrots.java
@@ -0,0 +1,13 @@
+import	java.util.Scanner;
+
+public class SolvingForCarrots {
+
+	public static void main(String[] args) {
+		Scanner input	=	new Scanner(System.in);
+		int	a = input.nextInt();
+			a = input.nextInt();
+		System.out.print(a);
+		return;
+	}
+
+}
