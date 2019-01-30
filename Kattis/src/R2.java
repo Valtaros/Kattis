82fc705c0a83d7c7f0055e542d1e1ebc2652e031
Fabian <fabian.grun@gmx.de>
2019-01-30 21:05:10 +0100

    "R2" solved

---
+++ Kattis/src/R2.java
@@ -0,0 +1,22 @@
+import	java.util.Scanner;
+
+public class R2 {
+
+	public static void main(String[] args) {
+		
+		//	initialize input Scanner
+		Scanner input	=	new Scanner(System.in);
+		
+		//	get input
+		int 	r1		=	input.nextInt();
+		int		s		=	input.nextInt();
+		
+		//	return result
+		System.out.print(((2*s)-r1));
+		
+		//	end program
+		input.close();
+		return;
+	}
+
+}
