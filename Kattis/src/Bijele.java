35452cccc53f0894358dd4459e83ad1989d6e961
Fabian <fabian.grun@gmx.de>
2019-01-31 02:40:34 +0100

    "Bijele" solved

---
+++ Kattis/src/Bijele.java
@@ -0,0 +1,29 @@
+import	java.util.Scanner;
+
+public class Bijele
+{
+
+	public static void main(String[] args)
+	{
+		//	initialize input Scanner
+		Scanner	input		=	new Scanner(System.in);
+		
+		//	generate output
+		System.out.print(	( 1-input.nextInt() ));
+		System.out.print(" ");
+		System.out.print(	( 1-input.nextInt() ));
+		System.out.print(" ");
+		System.out.print(	( 2-input.nextInt() ));
+		System.out.print(" ");
+		System.out.print(	( 2-input.nextInt() ));
+		System.out.print(" ");
+		System.out.print(	( 2-input.nextInt() ));
+		System.out.print(" ");
+		System.out.print(	( 8-input.nextInt() ));
+		
+		//	end program
+		input.close();
+		return;
+	}
+
+}
