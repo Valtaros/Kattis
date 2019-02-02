460d2740d1b26e847e4c5e5141550c942aacc042
Fabian <fabian.grun@gmx.de>
2019-02-02 21:43:08 +0100

    made a template

---
+++ Kattis/src/Template.java
@@ -0,0 +1,16 @@
+import	java.util.Scanner;
+
+public class Template {
+
+	public static void main(String[] args) {
+		//	initialize input scanner
+		Scanner	input	=	new Scanner(System.in);
+		
+		
+		
+		//	end program
+		input.close();
+		return;
+	}
+
+}
