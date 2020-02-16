7258004ac049d7330419120907f16e677798bfed
Fabian <fabian.grun@gmx.de>
2020-02-16 22:58:58 +0100

    solved FizzBuzz

---
+++ Kattis/src/solved/FizzBuzz.java
@@ -0,0 +1,30 @@
+package solved;
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+		
+		int x = inputScanner.nextInt();
+		int y = inputScanner.nextInt();
+		int n = inputScanner.nextInt();
+		
+		inputScanner.close();
+		
+		for(int i = 1; i <= n; i++) {
+			if(i % x == 0 || i % y == 0) {
+				if(i % x == 0) {
+					System.out.print("Fizz");
+				}
+				if(i % y == 0) {
+					System.out.print("Buzz");
+				}
+			}else {
+				System.out.print(i);
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
