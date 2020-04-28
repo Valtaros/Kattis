c8209fedb236c780958599a0a04fa79c5388b871
Fabian <fabian.grun@gmx.de>
2020-04-29 01:16:23 +0200

    solved problem "Quick Estimate"

---
+++ Kattis/src/solved/QuickEstimate.java
@@ -0,0 +1,19 @@
+//This is a solution for the problem "Quick Estimate" from open.kattis.com
+//link to problem: https://open.kattis.com/problems/quickestimate
+package solved;
+
+import java.util.Scanner;
+
+public class QuickEstimate {
+	
+	public static void main(String[] args) {
+		//initialize input scanner
+		Scanner	inputScanner = new Scanner(System.in);
+		int testCases = inputScanner.nextInt();
+		for(int i = 0; i < testCases; i++) {
+			System.out.println(inputScanner.next().length());
+		}
+		//close Scanner
+		inputScanner.close();
+	}
+}
